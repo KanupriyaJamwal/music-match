b15426cc63972d92c93bf646ed8836ee7f49cbf1
Kanupriya Jamwal <kanupriyajamwal15@gmail.com>
2025-04-04 23:09:59 +0100

    change port to 8000

--- server/app.py
+++ server/app.py
@@ -100,7 +100,6 @@ def download_file(request_id, filename):
         
     return send_file(filepath, as_attachment=True)
 
-port = int(os.environ.get("PORT", 5001))  # Default to 5001 if PORT not set
+port = int(os.environ.get("PORT", 8000))  # Default to 5001 if PORT not set
 if __name__ == '__main__':
-    # Add host='0.0.0.0' to allow external connections
-    app.run(host='0.0.0.0', port=5001, debug=True)
+    app.run(host='0.0.0.0', port=8000, debug=True)  # Changed to 8000
