842cccb5163b2d027703ac91e052fb05a9230d2f
Kanupriya Jamwal <kanupriyajamwal15@gmail.com>
2025-04-04 22:57:57 +0100

    changed port to 5001

--- server/app.py
+++ server/app.py
@@ -100,6 +100,6 @@ def download_file(request_id, filename):
         
     return send_file(filepath, as_attachment=True)
 
-port = int(os.environ.get("PORT", 5000))  # Default to 5000 if PORT not set
+port = int(os.environ.get("PORT", 5001))  # Default to 5000 if PORT not set
 if __name__ == '__main__':
     app.run(port=port)
